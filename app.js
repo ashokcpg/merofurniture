8f112f5bf7083d870648837f437c0de1d4a9258f
Ashok Chapagai <chapagaiashok@gmail.com>
2020-08-25 16:37:11 +0545

    JS file added

---
+++ app.js
@@ -0,0 +1,25 @@
+const cartBtn = document.querySelector(".cart-btn");
+const closeCart = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".claer-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+
+const cartTotal = document.querySelector(".cart-total");
+
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+// Cart items
+let cart = [];
+
+// Class responsible for getting the products...
+class Products {}
+
+// Display Products...
+
+class UI {}
+
+// Local Storage
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {});
