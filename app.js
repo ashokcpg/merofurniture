c564f04d87c855b55982a775b7af51b3bb675ba4
Ashok Chapagai <chapagaiashok@gmail.com>
2020-08-30 04:39:50 +0545

    Made a few changes in the file

--- app.js
+++ app.js
@@ -16,9 +16,18 @@ let cart = [];
 class Products {
 	async getProducts() {
 		try {
+			// Gets Local Data...
 			let result = await fetch("products.json");
 			let data = await result.json();
-			return data;
+			let products = data.items;
+			products = products.map((item) => {
+				const { title, price } = item.fields;
+				const { id } = item.sys;
+				const image = item.fields.image.fields.file.url;
+				// Returns Clean Object
+				return { title, price, id, image };
+			});
+			return products;
 		} catch (error) {
 			console.log(error);
 		}
@@ -37,5 +46,5 @@ document.addEventListener("DOMContentLoaded", () => {
 	const products = new Products();
 
 	// GET all products...
-	products.getProducts().then(console.log);
+	products.getProducts().then((data) => console.log(data));
 });
